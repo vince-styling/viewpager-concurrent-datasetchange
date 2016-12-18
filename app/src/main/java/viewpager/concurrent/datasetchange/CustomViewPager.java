2e71298c6503319d05cce8574c44de21fc2db1d1
lingyunxiao <lingyunxiao@cmcm.com>
2016-12-18 16:53:57 +0800

    fetch the ViewPager for test easily

--- app/src/main/java/viewpager/concurrent/datasetchange/CustomViewPager.java
+++ app/src/main/java/viewpager/concurrent/datasetchange/CustomViewPager.java
@@ -1,7 +1,6 @@
 package viewpager.concurrent.datasetchange;
 
 import android.content.Context;
-import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
 
 public class CustomViewPager extends ViewPager {
